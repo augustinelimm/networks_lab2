9b87f1bec1f753b4511a06c0563fde4cb2be8202
augustinelimm <augustinelim7@gmail.com>
2025-02-14 15:41:08 +0800

    update import details in main

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, Query, UploadFile, File, Header
+from fastapi import FastAPI, Depends, HTTPException, Query, Header
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
